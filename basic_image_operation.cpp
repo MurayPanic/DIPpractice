74a67a4bfb1da92e805605c9ee9c505d10ca580e
MurayPanic <lbwkabuto@gmail.com>
2019-08-05 11:41:14 +0800

    My first opencv program with c++ : basic image operation

--- basic_image_operation.cpp
+++ basic_image_operation.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include <opencv2/opencv.hpp>
 using namespace std;
+using namespace cv;
+
+
+
+
 
 int main(){
 
+    cv::Mat image = cv::imread("./amg_4_door.jpg");
+ 
+    String windowName ="AMG_4_Door";
+    namedWindow(windowName);
+    cv::imshow(windowName, image);
+    waitKey(0);
+    destroyWindow(windowName);
 
 	return 0;
 }
